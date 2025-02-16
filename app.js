4ebf2a0dc13839d4c955684b6fcbea16b80c7a27
krityagi <ktyagi0602@gmail.com>
2025-02-16 17:31:57 +0000

    Updated app.js

--- app.js
+++ app.js
@@ -13,7 +13,10 @@ const adminRoutes = require('./routes/admin');
 const app = express();
 const port = process.env.PORT || 3000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+// MongoDB connection string with authentication
+const MONGO_URI = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@mongo:27017/devopsduniya?authSource=admin`;
+
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error: ', err));
 
